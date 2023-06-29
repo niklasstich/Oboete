6e94d751369afd92e1ca5d17e811c06d7944ec0e
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2023-06-29 23:52:54 +0200

    Added Oboete.API.Shared

---
+++ Oboete.API/Oboete.API.Shared/EitherExtensions.cs
@@ -0,0 +1,9 @@
+﻿using LanguageExt;
+
+namespace Oboete.API.Shared;
+
+public static class EitherExtensions
+{
+    public static async Task<Either<TL, TR>> FlattenAsync<TL, TR>(this Task<Either<TL, Either<TL, TR>>> task) =>
+        (await task).Flatten();
+}
